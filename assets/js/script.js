ed7be7d3cf16dc425eb0bea4d43ec18ae25c5a2c
Justin Stone <justinstone@Justins-MacBook-Pro-2.local>
2022-05-31 23:34:14 -0400

    Added comments into javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
+// Setting the current day/month/year at the top of the screen
+
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
+// Selecting the container class for future reference
 var containerEl = $(".container");
-var input = $("#input");
 
+// Iterates through the hours 9 am to 5 pm
 for (var i = 9; i < 18; i++) {
     var text = $("<textarea>").attr('class', 'col-10');
     var time = $("<div>").attr('class', 'col-1 hour');
@@ -11,9 +14,9 @@ for (var i = 9; i < 18; i++) {
     var saveBtn = $("<button>").attr('class', 'col-1 saveBtn');
     saveBtn.text('Save');
 
+    // Set the colors based on the users current time
     if (i === moment().hour()) {
         text.attr("class", "present col-10");
-
     }
 
     if (i > moment().hour()) {
@@ -24,6 +27,7 @@ for (var i = 9; i < 18; i++) {
         text.attr("class", "past col-10");
     }
 
+    // Set the rows up with each time, a space to enter text and a save button
     if (i < 12){
         time.text(i + "AM");
         rows.append(time);
